a013ea320a725b20c4ec537bd29cb0867506ff59
clashivfx-creator <clashivfx@gmail.com>
2026-02-22 16:45:02 -0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '927443432987006');
+            fbq('init', '1594983758216504');
             fbq('track', 'PageView');
             fbq('track', 'ViewContent', {
               content_name: 'Catálogo VFX',
--- components/ui/AnimatedBackground.tsx
+++ components/ui/AnimatedBackground.tsx
@@ -8,9 +8,9 @@ const LiquidEther = dynamic(() => import('./LiquidEther'), { ssr: false });
 export const AnimatedBackground: React.FC = () => {
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden bg-black pointer-events-none transform-gpu">
-      <div className="absolute inset-0 w-full h-full opacity-40">
+      <div className="absolute inset-0 w-full h-full opacity-60">
         <LiquidEther
-          colors={['#050510', '#100520', '#0a0a15']}
+          colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
           mouseForce={14}
           cursorSize={100}
           isViscous
@@ -20,23 +20,23 @@ export const AnimatedBackground: React.FC = () => {
           resolution={0.5}
           isBounce={false}
           autoDemo
-          autoSpeed={0.3}
-          autoIntensity={1.8}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
           takeoverDuration={0.25}
           autoResumeDelay={3000}
-          autoRampDuration={1.2}
+          autoRampDuration={0.6}
         />
       </div>
-
+      
       {/* Gradientes de profundidad para suavizar los bordes */}
       <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-black pointer-events-none" />
-
+      
       {/* Mesh gradients adicionales para profundidad */}
-      <div
+      <div 
         className="absolute top-[-5%] left-[-5%] w-[50%] h-[50%] rounded-full bg-blue-900/5 blur-[40px] animate-float-slow will-change-transform transform-gpu"
         style={{ animationDuration: '30s' }}
       />
-      <div
+      <div 
         className="absolute bottom-[5%] right-[-5%] w-[40%] h-[40%] rounded-full bg-purple-900/5 blur-[50px] animate-float-slow will-change-transform transform-gpu"
         style={{ animationDuration: '35s', animationDelay: '-7s' }}
       />
